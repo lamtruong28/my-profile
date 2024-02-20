51acd30906a84a750d7af509d2fde9b21b0a25d8
lamtruong28 <lamtruongpk31@gmail.com>
2024-02-20 22:38:31 +0700

    update 20-02-2024

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,18 +16,18 @@ const Home = () => {
             <div className={cx("content")}>
                 <div className={cx("text")}>
                     <h1 className={cx("title")}>
-                        I'm Tran Lam Truong <span>Web Developer !</span>
+                        Trần Lâm Trường <span>Web Developer !</span>
                     </h1>
                     <p className={cx("greet")}>
-                        Xin chào mọi người! Mình là Trường, là sinh viên vừa mới
-                        tốt nghiệp trường Cao đẳng Lý Tự Trọng. Ngoài việc học
-                        kiến thức ở trường, mình còn tự học thêm các kiến thức
-                        khác từ nhiều website khác nhau. Cộng đồng F8 Fullstack
-                        của anh Sơn là một trang mình học được rất nhiều kiến
-                        thức bổ ích. Mình đã hoàn thành các khóa học HTML, CSS,
-                        JavaScript, ReactJS,... Khi học ở trường và F8, mình có
-                        một vài dự án cá nhân, mong mọi người xem và góp ý cho
-                        mình, để mình ngày càng hoàn thiện hơn.
+                        Xin chào mọi người! Mình là Trường, một người đam mê lập
+                        trình. Ngoài việc học kiến thức ở trường, mình còn tự
+                        học thêm các kiến thức khác từ nhiều website khác nhau.
+                        Cộng đồng F8 Fullstack của anh Sơn là một trang mình học
+                        được rất nhiều kiến thức bổ ích. Mình đã hoàn thành các
+                        khóa học HTML, CSS, JavaScript, ReactJS,... Khi học ở
+                        trường và F8, mình có một vài dự án cá nhân, mong mọi
+                        người xem và góp ý cho mình, để mình ngày càng hoàn
+                        thiện hơn.
                         <span className={cx("thanks")}>Mình xin cảm ơn!</span>
                     </p>
                     <div className={cx("button")}>
